478f7f8e6612cf0244bd20dbd5d75679e25e51ec
Dietmar Moritz <didi.moritz@gmail.com>
2018-10-22 22:54:46 +0200

    add styles and test stuff

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,106 @@ import { BranchSummary } from "simple-git/typings/response";
 
 const git: GitHelper = new GitHelper();
 
-term.grabInput();
+const PROGESS_AFTER_FETCH: number = 10;
+const TITLE: string = "Git Cleanup";
 
-term.on('key', function (name, matches, data) {
-    if (name === 'CTRL_C' || name === 'q' || name === 'Q') {
-        term.fullscreen(false);
-        process.exit();
+let exitAsap: boolean = false;
+
+function init(): void {
+    term.fullscreen(true);
+    term.windowTitle("Oh yeah!");
+    
+    showTitle();
+    
+    term.color(1, "Servas\n");
+    term.white("###ABCDEFG###\n");
+    term.brightWhite("###ABCDEFG###\n");
+    term.color(15, "###ABCDEFG###\n");
+    term.brightRed("Hello, folks!\n");
+    term.brightBlue("It is: " + term.width + "x" + term.height + "\n");
+
+    loadRepositories();
+}
+
+function showTitle(): void {
+    let textStartPos: number = Math.ceil((term.width - TITLE.length + 1) / 2);
+    if (textStartPos < 1) {
+        textStartPos = 1;
     }
-});
 
-term.fullscreen(true);
+    for (let i = 1; i <= textStartPos - 2; i++) {
+        term.white("#");
+    }
 
-term.showBottomMenu();
+    term.column(textStartPos);
+    term.bold.brightBlue(TITLE);
+    term.right(1);
 
-term.windowTitle("Oh yeah!");
+    for (let i = textStartPos + TITLE.length + 1; i <= term.width; i++) {
+        term.white("#");
+    }
 
-term.red("Hello, folks!\n");
+    term.nextLine();
+}
 
-term.blue("It is: " + term.width + "x" + term.height + "\n");
+function initControls(): void {
+    term.grabInput();
 
-git.readLocalRepos().then((result: Branch[]) => {
-    term.blue("Mkay!\n");
-    result.forEach((branch: Branch) => {
-        term(branch.name);
-        term.blue("\t" + (branch.isLocal ? "L" : " "));
-        term.green("\t" + (branch.isRemote ? "R" : " "));
-        term.red("\t" + (branch.isCurrent ? "C" : " "));
-        term.nextLine();
+    term.on('key', function (name, matches, data) {
+        if (name === 'CTRL_C' || name === 'q' || name === 'Q') {
+            exitAsap = true;
+            term.fullscreen(false);
+            process.exit();
+        }
     });
-});
+}
+
+function loadRepositories(): void {
+    term.showProgess(0);
+
+    git.readLocalRepos().then((result: Branch[]) => {
+        term.showProgess(PROGESS_AFTER_FETCH);
+
+        term.blue("Mkay!\n");
+        for (let i = 0; i < result.length; i++) {
+            const branch: Branch = result[i];
+            term(branch.name);
+            term.blue("\t" + (branch.isLocal ? "L" : " "));
+            term.green("\t" + (branch.isRemote ? "R" : " "));
+            term.red("\t" + (branch.isCurrent ? "C" : " "));
+            term.nextLine();
+
+            const percentage: number = (i + 1) * (100 - PROGESS_AFTER_FETCH) / result.length + PROGESS_AFTER_FETCH;
+
+            term.showProgess(percentage);
+
+            sleep(200);
+        }
+        // result.forEach((branch: Branch) => {
+        //     term(branch.name);
+        //     term.blue("\t" + (branch.isLocal ? "L" : " "));
+        //     term.green("\t" + (branch.isRemote ? "R" : " "));
+        //     term.red("\t" + (branch.isCurrent ? "C" : " "));
+        //     term.nextLine();
+        //     sleep(500);
+        // });
+
+        initControls();
+        initMenu();
+    });
+}
+
+function initMenu(): void {
+    term.showBottomMenu();
+}
+
+init();
+
+// For testing only
+function sleep(ms: number): void {
+    var date: number = Date.now();
+    var curDate: number = Date.now();
+    do {
+        curDate = Date.now();
+    } while ((curDate - date) < ms);
+}
--- src/terminal.ts
+++ src/terminal.ts
@@ -1,14 +1,41 @@
 var term = require("terminal-kit").terminal;
 
-term.foo = () => {
-    term.moveTo.red(10, 10, "Alrighty then!\n");
+term.showBottomMenu = () => {
+    term.showInBottomLine(() => {
+        term.eraseLine();
+        term.bgBrightWhite.brightRed("Q");
+        term.white("uit");
+    });
 };
 
-term.showBottomMenu = () => {
+term.centerText = (termChain: any, text: string) => {
+    let startPos: number = Math.floor((term.width - text.length) / 2);
+    startPos = startPos < 0 ? 0 : startPos;
+
+    term.column(startPos);
+    termChain(text);
+};
+
+term.showProgess = (percentage: number) => {
+    const numberOfDoneChars: number = Math.round((term.width - 2) * percentage / 100);
+    const numberOfUndoneChars: number = term.width - 2 - numberOfDoneChars;
+
+    term.showInBottomLine(() => {
+        term.white("[");
+        for (var i = 0; i < numberOfDoneChars; i++) {
+            term.brightWhite("#");
+        }
+        for (var i = 0; i < numberOfUndoneChars; i++) {
+            term.white("-");
+        }
+        term.white("]");
+    });
+};
+
+term.showInBottomLine = (displayFunc: () => void) => {
     term.saveCursor();
     term.moveToBottomLine();
-    term.bgWhite.red("Q");
-    term.white("uit");
+    displayFunc();
     term.restoreCursor();
 };
 
